be8a20df45e59f1301aed6a6c947f0809b7ce424
Elvis Wanjiru <elvis.gatukui@student.moringaschool.com>
2022-10-25 21:08:32 +0300

    Done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative "./user"
 
+class Student < User
+
+attr_reader :knowledge
+
+    def initialize
+        @knowledge = []
+end
+
+def learn(student)
+    @knowledge << student
+end
+
+def knowledge
+    @knowledge
+end
 end
